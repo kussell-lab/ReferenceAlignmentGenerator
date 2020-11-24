0a68df10fb74311b18ffb59e6a42857d17b834ae
Asher Preska Steinberg <asherps@gmail.com>
2020-11-24 16:33:59 -0500

    APS156 edit to fetchnmap package

--- fetchnmap/map/functions.py
+++ fetchnmap/map/functions.py
@@ -11,7 +11,7 @@ def map2reference(accession, wrkdir, reference):
     read2 = os.path.join(wrkdir, accession + "_2.fastq")
     ##sort the alignment file
     bam = os.path.join(wrkdir, accession + ".sorted.bam")
-    os.system("smalt map -n 1 %s %s %s | samtools sort -T /temp -o %s" % (reference, read1, read2, bam))
+    os.system("smalt map -n 1 %s %s %s | samtools sort -o %s" % (reference, read1, read2, bam))
     ##multi-way pileup to consensus genome
     pile = os.path.join(wrkdir, accession+".pileup.fasta")
     os.system("samtools mpileup -f %s %s | GenomicConsensus --fna_file %s > %s" % (reference, bam, reference, pile))
