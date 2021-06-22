b282478984889fa6afaa1e94abe035cde1eea7e6
Asher Preska Steinberg <asherps@gmail.com>
2021-06-22 15:23:58 -0400

    APS202 submission script commits

---
+++ hpc_submissions/APS202BinsNM.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+DATE=210522_NM
+ARCHIVE=/scratch/aps376/recombo/APS183_NM_Archive
+OUTDIR=/scratch/aps376/recombo/APS202_NM_Archive
+MSA=${ARCHIVE}/MSA_NM_MASTER_GAPFILTERED
+list=${ARCHIVE}/strain_list
+SUBMITDIR=/scratch/aps376/recombo/APS202genebins/${DATE}_submissions
+SLURMDIR=/scratch/aps376/recombo/APS202genebins/${DATE}_slurm
+
+mkdir -p ${SUBMITDIR}
+mkdir -p ${SLURMDIR}
+mkdir -p ${OUTDIR}
+
+bins=(0 15 55 95)
+for i in {0..2}
+do
+  min=${bins[$i]}
+  max=${bins[$i+1]}
+  echo "submitting ${min}-${max}"
+  jobfile=${SUBMITDIR}/bin_${min}-${max}.sh
+  echo "#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=4
+#SBATCH --time=4:00:00
+#SBATCH --mem=4GB
+#SBATCH --job-name=bin${min}-${max}
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=aps376@nyu.edu
+#SBATCH --output=${SLURMDIR}/slurm%j_bin_${min}-${max}.out
+
+
+##INPUTS
+
+echo \"Loading modules.\"
+module load go/1.15.7
+module load singularity/3.6.4
+
+##aliases for singularity
+
+##Making the AssemblyAlignmentGenerator and ReferenceAlignmentGenerator in path
+echo \"Making everything in path.\"
+#mcorr
+export PATH=\$PATH:\$HOME/go/bin:\$HOME/.local/bin
+
+#ReferenceAlignmentGenerator
+export PATH=\$PATH:~/opt/AssemblyAlignmentGenerator/
+export PATH=\$PATH:~/opt/ReferenceAlignmentGenerator
+
+##set perl language variable; this will give you fewer warnings
+export LC_ALL=C
+
+echo \"let's rock\"
+mkdir -p ${OUTDIR}
+
+GeneBins ${MSA} ${list} ${min} ${max} ${OUTDIR}/bin_${min}-${max} --threads=8 --num-cpu=4" > $jobfile
+    sbatch "$jobfile"
+    echo "I'm taking a 1 second break"
+    sleep 1 #pause the script for a second so we don't break the cluster with our magic
+done
+
+
---
+++ hpc_submissions/APS202clusterSplitNG.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=4
+#SBATCH --time=4:00:00
+#SBATCH --mem=4GB
+#SBATCH --job-name=NG_clustersplit
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=aps376@nyu.edu
+#SBATCH --output=NG_clustersplit_slurm%j.out
+
+
+##INPUTS
+cluster_dict=/scratch/aps376/recombo/APS171_NG_Archive/cluster_list
+msa_dir=/scratch/aps376/recombo/APS202_NG_Archive
+out=${msa_dir}
+
+echo "Loading modules."
+module load go/1.15.7
+
+##aliases for singularity
+
+##Making the AssemblyAlignmentGenerator and ReferenceAlignmentGenerator in path
+echo "Making everything in path."
+#mcorr
+export PATH=$PATH:$HOME/go/bin:$HOME/.local/bin
+
+#ReferenceAlignmentGenerator
+export PATH=$PATH:~/opt/AssemblyAlignmentGenerator/
+export PATH=$PATH:~/opt/ReferenceAlignmentGenerator
+
+##set perl language variable; this will give you fewer warnings
+export LC_ALL=C
+
+echo "let's rock"
+for bin in "0-15" "15-55" "55-95";
+do
+  mkdir -p ${out}/bin_${bin}
+  clusterSplit ${msa_dir}/bin_${bin}/MSA_${bin} ${out}/bin_${bin} ${cluster_dict} --num-cpu=4
+done
+
